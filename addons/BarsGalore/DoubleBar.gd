32ecf74835936bf90cb485c22da0e62ad169e07f
Swyftl <juniorjonswift@gmail.com>
2026-08-08 23:41:04 +0100

    Initial Commit

---
+++ addons/BarsGalore/DoubleBar.gd
@@ -0,0 +1,34 @@
+@tool
+@icon("res://addons/BarsGalore/StaminaBar.svg")
+
+class_name DoubleBar
+extends Control
+
+@export var value_one : float = 100
+@export var value_two : float = 100
+@export var value_one_max : float = 100
+@export var value_two_max : float = 100
+
+@export_category("Visual")
+@export var bar_one_color : Color = Color.GREEN
+@export var bar_two_color : Color = Color.BLUE
+@export var bar_one_width : float = 10.0
+@export var bar_two_width : float = 5.0
+@export var antialiased : bool = true
+@export var bar_one_size_offset : float = 0.0
+@export var bar_two_size_offset : float = 0.0
+
+func _process(_delta):
+	queue_redraw()
+
+func _draw():
+	var rect_height = size.y / 2
+	
+	var rect_one_percentage = value_one/value_one_max
+	var rect_two_percentage = value_two/value_two_max
+	
+	var rect_one = Rect2i(bar_one_size_offset/2, rect_height-(bar_one_width/2), (rect_one_percentage)*(size.x - bar_one_size_offset), bar_one_width)
+	var rect_two = Rect2i(bar_two_size_offset/2, rect_height-(bar_two_width/2), (rect_two_percentage)*(size.x - bar_two_size_offset), bar_two_width)
+	
+	draw_rect(rect_one, bar_one_color, true, -1.0, antialiased)
+	draw_rect(rect_two, bar_two_color, true, -1.0, antialiased)
---
+++ addons/BarsGalore/StaminaRing.gd
@@ -0,0 +1,43 @@
+@tool
+@icon("res://addons/BarsGalore/StaminaBar.svg")
+
+class_name StaminaBar
+extends Control
+
+@export var value : float = 100
+@export var max_value : float = 100
+@export_group("Visual")
+@export var point_count : int = 20
+@export var color : Color = Color.ALICE_BLUE
+@export var antialiased : bool = true
+@export var width : float = 2.0
+
+func _enter_tree():
+	pass
+
+func _process(delta: float) -> void:
+	queue_redraw()
+
+func _draw() -> void:
+	var radius : float = min(size.x - (width), size.y - (width)) / 2.0
+	var center := size / 2.0
+
+	# Prevent division by zero and keep the value in a valid range.
+	var ratio := 0.0
+	if max_value > 0.0:
+		ratio = clamp(value / max_value, 0.0, 1.0)
+
+	# draw_arc uses radians, so TAU is one complete circle.
+	var start_angle := -PI / 2.0
+	var end_angle := start_angle + ratio * TAU
+
+	draw_arc(
+		center,
+		radius,
+		start_angle,
+		end_angle,
+		point_count,
+		color,
+		width,
+		antialiased
+	)
