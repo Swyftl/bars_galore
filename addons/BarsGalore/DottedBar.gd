8f7560d4ecf910af2869348c8a0f99c6b368a5e3
Swyftl <juniorjonswift@gmail.com>
2026-08-20 14:38:47 +0100

    Changed DottedBar to space based on how many fit, instead of set amount

--- addons/BarsGalore/DottedBar.gd
+++ addons/BarsGalore/DottedBar.gd
@@ -13,18 +13,42 @@ extends Control
 @export var antialiased : bool = true
 @export var color : Color = Color.GREEN
 @export var dot_radius : float = 5
-@export var max_dots : int = 10
 @export var dot_spacing : float = 5
 
 func _process(_delta) -> void:
 	queue_redraw()
-	
-func _draw():
-	var bar_percentage: float = value/maxValue
-	var shown_dots : int = bar_percentage * max_dots
 
-	var spacing: float = (size.x / max_dots) + dot_spacing
-	print(spacing)
+func _draw():
+	var size: Vector2 = get_size()
+
+	var dot_diameter: float = dot_radius * 2
+	var box_width: float = size.x
+
+	var max_dots_no_spacing: int = floor(box_width / dot_diameter)
 	
-	for dot_count in shown_dots:
-		draw_circle(Vector2((spacing*dot_count), size.y/2), dot_radius, color, true, -1.0, antialiased)
+	var dot_amount: int = floor((box_width + dot_spacing) / (dot_diameter + dot_spacing))
+	dot_amount = max(1, dot_amount)
+
+	var actual_spacing: float = 0.0
+	if dot_amount > 1:
+		actual_spacing = (box_width - (dot_amount * dot_diameter)) / (dot_amount - 1)
+	else:
+		actual_spacing = 0.0
+
+	if actual_spacing < dot_spacing:
+		dot_amount = floor((box_width + dot_spacing) / (dot_diameter + dot_spacing))
+		dot_amount = max(1, dot_amount)
+		if dot_amount > 1:
+			actual_spacing = (box_width - (dot_amount * dot_diameter)) / (dot_amount - 1)
+		else:
+			actual_spacing = 0.0
+
+	var bar_percentage: float = value / maxValue
+	var shown_dots: int = ceil(dot_amount * bar_percentage)
+
+	for i in range(shown_dots):
+		var x_pos: float = dot_radius + (i * (dot_diameter + actual_spacing))
+		if x_pos + dot_radius > box_width:
+			break
+
+		draw_circle(Vector2(x_pos, size.y / 2), dot_radius, color, true, -1.0, antialiased)   
